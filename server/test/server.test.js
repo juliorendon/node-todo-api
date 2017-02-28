c79242323ca893f57688ee30595dfbe98bbebf89
Julio Rendon <juliorendon@Julios-MacBook-Pro.local>
2017-02-28 15:55:53 +0100

    added list route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -7,10 +7,18 @@ const {Todo} = require('./../models/todo');
 // beforeEach function gets called before each test(individually) its execueted.
 // Its usefull to initialize things before every test.
 // In this case Todo.remove({}), removes all todos on the Todos Collection.
+const todos =  [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}];
+
 beforeEach((done) => {
   Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => {
     done();
-  });
+  })
 });
 
 describe('POST /todos', () => {
@@ -29,7 +37,7 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -48,9 +56,21 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
-        }).catch((e) => done(e));
+        }).catch((e) => done(e)); // We put a catch because its a asyncronous call
       });
+  });
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {  // Mocha
+    request(app)    // Supertest
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   });
 });
